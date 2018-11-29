f782a65fe3bcd439c2cf0278c4469108e3d71359
Spindlyskit <josh.lavelle1@gmail.com>
2018-11-29 14:46:10 +0000

    Rename deploy_key.enc to deploy-key.enc

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -41,10 +41,10 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in travis/deploy-key.enc -out deploy-key -d
-chmod 600 deploy-key
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in travis/deploy-key.enc -out ../deploy_key -d
+chmod 600 ../deploy_key
 eval `ssh-agent -s`
-ssh-add deploy-key
+ssh-add ../deploy_key
 
 # Checkout the repo in the target branch so we can build docs and push to it
 TARGET_BRANCH="gh-pages"
