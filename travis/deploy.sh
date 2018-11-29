de7169370f233ba5cf4923a1942fcd09b658c1e3
Spindlyskit <josh.lavelle1@gmail.com>
2018-11-29 15:55:36 +0000

    Attempt to fix travis build

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -37,14 +37,7 @@ SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
 # Decrypt and add the ssh key
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in travis/deploy-key.enc -out ../deploy_key -d
-chmod 600 ../deploy_key
-eval `ssh-agent -s`
-ssh-add ../deploy_key
+ssh-add travis/deploy_key
 
 # Checkout the repo in the target branch so we can build docs and push to it
 TARGET_BRANCH="gh-pages"
