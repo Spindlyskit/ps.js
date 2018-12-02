b1caff3bed9e05b4808d302faee988b0a4f12ea0
Spindlyskit <josh.lavelle1@gmail.com>
2018-12-02 20:46:56 +0000

    add echos

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -37,16 +37,19 @@ SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
 # Decrypt and add the ssh key
+echo "Decrypting key"
 openssl aes-256-cbc -K $encrypted_26b4962af0e7_key -iv $encrypted_26b4962af0e7_iv
   -in travis/deploy-key.enc -out deploy-key -d
 eval "$(ssh-agent -s)"
 ssh-add deploy_key
+echo "Key added to agent"
 
 # Checkout the repo in the target branch so we can build docs and push to it
 TARGET_BRANCH="gh-pages"
 git clone $REPO out -b $TARGET_BRANCH
 
 # Commit and push
+echo "Pushing"
 cd docs
 git add .
 git config user.name "Travis CI"
@@ -57,3 +60,5 @@ git push $SSH_REPO $TARGET_BRANCH
 # Clean up...
 cd ..
 rm -rf out
+
+echo "complete"
